3b4cf3e0737951d069a6f8e7cdd4733733beae8d
Matt McManus <mattmcmanus@gmail.com>
2012-04-27 15:52:21 -0400

    Initial commit - nslookup support

---
+++ README.md
@@ -0,0 +1,27 @@
+# icanhaswww
+
+Check to see if a site is available on multiple networks
+
+## Getting Started
+Install the module with: `npm install icanhaswww`
+
+```javascript
+var icanhaswww = require('icanhaswww');
+icanhaswww.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Matt McManus  
+Licensed under the MIT license.
---
+++ test/honeybadger.test.js
@@ -0,0 +1,40 @@
+var HB = require('../lib/honeybadger.js'),
+    honeybadger = new HB();
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['honeybadger'] = {
+  setUp: function(done) {
+    done();
+  },
+  'test from localhost to google.com': function(test) {
+    test.expect(2);
+    // tests here
+    honeybadger.badger('google.com', function(err, results){
+      
+      test.equal( err, null, 'should be null.' );
+      test.ok( results[0].nslookup.status, 'should properly resolve dns' );
+      test.done();
+    });
+    
+    
+  }
+};
